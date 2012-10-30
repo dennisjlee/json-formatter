2494df781ccb1b8fba48d110f5258fba64096a81
André Figueira <andre.figueira@me.com>
2012-10-30 23:42:31 +0000

    Made demo work, added closed option, added spacing to the code to make it more readable change coding style to camel case.

--- src/JSONFormatter.js
+++ src/JSONFormatter.js
@@ -1,156 +1,234 @@
+JSONFormatter = (function(){
 
-JSONFormatter = (function() {
-
-  var init = function( json, options ) { 
+  var init = function(json, options){ 
     
     // default settings
-    var settings = $.extend( {
-      'appendTo' : 'body',
-      'list_id' : 'json',
-      'collapse' : false
+    var settings = $.extend({
+      'element' : 'body',
+      'listID' : 'json',
+      'collapse' : false,
+      'closed': false
     }, options);
     
     var loopCount = 0;
     
-    loopObjectOfObjects = function(json2, ulId) {
-      $.each(json2, function(k3, v3) {
+    loopObjectOfObjects = function(json2, ulId) 
+    {
+    
+      $.each(json2, function(k3, v3){
+      
         // object of objects
-        if(typeof v3 == 'object') {
-          $('#' + settings.list_id + ' #' + ulId).append('<li><span>{</span> <ul id="' + ulId + '-' + k3 + '"></ul></li>');
-          $.each(v3, function(k4, v4) {
-            if(typeof v4 == 'object' && v4 != null) {
-              $('#' + settings.list_id + ' #' + ulId + '-' + k3).append('<li>' + k4 + ' <span>{</span> <ul id="'+k4+'-'+loopCount+'"></ul></li>');
+        if(typeof v3 == 'object'){
+        
+          $('#' + settings.listID + ' #' + ulId).append('<li><span>{</span> <ul id="' + ulId + '-' + k3 + '"></ul></li>');
+          
+          $.each(v3, function(k4, v4){
+          
+            if(typeof v4 == 'object' && v4 != null) 
+            {
+            
+              $('#' + settings.listID + ' #' + ulId + '-' + k3).append('<li>' + k4 + ' <span>{</span> <ul id="' + k4 + '-' + loopCount + '"></ul></li>');
               loopAgain(v4, k4, k4 + '-' + loopCount);
+            
             }
-            else {
-              $('#' + settings.list_id + ' #' + ulId + '-' + k3).append('<li>' + k4 + ': ' + v4 + '</li>');
+            else 
+            {
+            
+              $('#' + settings.listID + ' #' + ulId + '-' + k3).append('<li>' + k4 + ': ' + v4 + '</li>');
+              
             }
 
           });
+          
         } 
-        else {
+        else 
+        {
+        
           // normal array
-          $('#' + settings.list_id + ' #' + ulId).append('<li>' + v3 + '</li>')
+          $('#' + settings.listID + ' #' + ulId).append('<li>' + v3 + '</li>');
+          
         }
+        
       });
+      
     },
 
-    loopAgain = function(v, k, ulId) {
+    loopAgain = function(v, k, ulId){
+    
       loopCount++;
-      $.each(v, function(nextKey, nextVal) {
+      
+      $.each(v, function(nextKey, nextVal){
+      
         var nextListId = nextKey + '-' + loopCount;
         var newList = '<ul id="' + nextListId + '"></ul>';
-        if(nextVal != null && typeof nextVal == 'object') {
-          if(nextVal.length == 0) {
+        
+        if(nextVal != null && typeof nextVal == 'object') 
+        {
+        
+          if(nextVal.length == 0)
+          {
+          
             // an empty object, just output that
-            $('#' + settings.list_id + ' #' + ulId).append('<li><i>' + nextKey + ':</i> []</li>');
-          } 
-          else if(nextVal.length >= 1) {
-            // an object of objects
-            $('#' + settings.list_id + ' #' + ulId).append('<li><b>' + nextKey + ':</b> <span>[</span> ' + newList + '</li>');
-            loopObjectOfObjects(nextVal, nextListId);
-          }
-          else if(nextVal.length == undefined) {
-            // next node
-            $('#' + settings.list_id + ' #' + ulId).append('<li><b>' + nextKey + ':</b> <span>{</span> ' + newList + '</li>');
-            loopAgain(nextVal, nextKey, nextListId);
-          }        
-        }
-        else {
-          // value|key
-          // if(nextKey.val == undefined) {
-          //   $('#' + settings.list_id + ' #' + ulId).append('<li>' + nextVal + '</li>');
-          //   
-          // }
-          // else {
-            $('#' + settings.list_id + ' #' + ulId).append('<li><i>'+ nextKey + ':</i> ' + nextVal + '</li>');
+            $('#' + settings.listID + ' #' + ulId).append('<li><i>' + nextKey + ':</i> {}</li>');
             
-          // }
+          } 
+          else if(nextVal.length >= 1)
+          {
+          
+            // an object of objects
+            $('#' + settings.listID + ' #' + ulId).append('<li><b>' + nextKey + ':</b> <span>{</span> ' + newList + '</li>');
+            loopObjectOfObjects(nextVal, nextListId);
+            
+          }
+          else if(nextVal.length == undefined) 
+          {
+          
+            // next node
+            $('#' + settings.listID + ' #' + ulId).append('<li><b>' + nextKey + ':</b> <span>{</span> ' + newList + '</li>');
+            loopAgain(nextVal, nextKey, nextListId);
+            
+          }  
+                
         }
+        else
+        {
+        
+            $('#' + settings.listID + ' #' + ulId).append('<li><i>'+ nextKey + ':</i> ' + nextVal + '</li>');
+            
+        }
+        
       });
+      
     },
     
-    addClosingBraces = function() {
-      $('#' + settings.list_id + ' span').each(function() {
+    addClosingBraces = function()
+    {
+    
+      $('#' + settings.listID + ' span').each(function(){
+      
         var closingBrace = '<span>}</span>';
-        if($(this).text() == "[") {
-          closingBrace = '<span>]</span>';
+        
+        if($(this).text() == "{")
+        {
+        
+          closingBrace = '<span>}</span>';
+          
         }
+        
         $(this).parent().find('ul').eq(0).after(closingBrace);
-      });      
+        
+      });
+            
     };
 
-    var jsonList = $('<ul id="' + settings.list_id + '" />');
+    var jsonList = $('<ul id="' + settings.listID + '" />');
 
-    $(settings.appendTo).append(jsonList);
+    $(settings.element).html(jsonList);
 
-    $.each(json, function(key, val) {
+    $.each(json, function(key, val){
       
+      if(val != null && typeof val == 'object') 
+      {
       
-      
-      if(val != null && typeof val == 'object') {
         var goObj = false;
         var goArray = false;
         var nk = '';
-        $.each(val, function(nextKey, nextVal) {
         
-          if(nextVal != null && typeof nextVal == 'object') {
-            if(nextVal.length == undefined) {
+        $.each(val, function(nextKey, nextVal){
+        
+          if(nextVal != null && typeof nextVal == 'object') 
+          {
+          
+            if(nextVal.length == undefined) 
+            {
+            
               goObj = true;
               nk = nextKey;
+              
             }
-            else {
+            else 
+            {
+            
               goObj = false;
+              
             }
+            
           }
-          else {
-            // console.log('nextVal ' + nextVal);
+          else 
+          {
+          
             goArray = true;
+            
           }
+          
         });
 
-        if(goObj) {
-          $('#' + settings.list_id).append('<li><b>' + key + ':</b> <span>[</span><ul id="' + nk + '-' + loopCount + '"></ul></li>');
+        if(goObj) 
+        {
+        
+          $('#' + settings.listID).append('<li><b>' + key + ':</b> <span>{</span><ul id="' + nk + '-' + loopCount + '"></ul></li>');
           loopObjectOfObjects(val, nk + '-' + loopCount);
+        
         }
-        else if(goArray) {
-          $('#' + settings.list_id).append('<li><b>' + key + ':</b> <span>[</span><ul id="' + nk + '-' + loopCount + '"></ul></li>');
+        else if(goArray) 
+        {
+        
+          $('#' + settings.listID).append('<li><b>' + key + ':</b> <span>{</span><ul id="' + nk + '-' + loopCount + '"></ul></li>');
           loopAgain(val, nk, nk + '-' + loopCount);
+          
         }
-        else {
-          $('#' + settings.list_id).append('<li><b>' + key + ':</b> <span>{</span><ul id="' + key + '-' + loopCount + '"></ul></li>');
-          loopAgain(val, key, key + '-' + loopCount);              
+        else 
+        {
+        
+          $('#' + settings.listID).append('<li><b>' + key + ':</b> <span>{</span><ul id="' + key + '-' + loopCount + '"></ul></li>');
+          loopAgain(val, key, key + '-' + loopCount);  
+                      
         }
         
       }
       else {
-        $('#' + settings.list_id).append('<li><i>' + key + ':</i> ' + val + '</li>');
+        $('#' + settings.listID).append('<li><i>' + key + ':</i> ' + val + '</li>');
       }
     });
     
     addClosingBraces();
     
-    if(settings.collapse) {
-      addToggles(settings.list_id);      
+    if(settings.collapse) 
+    {
+    
+      addToggles(settings.listID);      
+      
     }
     
   },
   
-  addToggles = function( listId ) {
-    $('#' + listId + " > li").find('ul').each(function() {
-      $(this).parent().find('span').eq(0).after('<span class="toggle fake-link"> - </span>');
+  addToggles = function(listId){
+  
+    $('#' + listId + " > li").find('ul').each(function(){
+    
+      $(this).parent().find('span').eq(0).before('<span class="toggle fake-link"> &#9660; </span>');
+      
     });
-
-    $('.toggle').next().slideUp().end().text(' + ');
 
     $('.toggle').live('click', function() {
-      if($(this).next().is(":visible")) {
-        $(this).next().slideUp().end().text(' + ');
+    
+      if($(this).next().next().is(":visible"))
+      {
+      
+        $(this).next().next().hide();
+        $(this).html(' &#9658; ')
+        
       }
-      else {
-        $(this).next().slideDown().end().text(' - ');
+      else 
+      {
+      
+        $(this).next().next().show();
+        $(this).html(' &#9660; ');
+        
       }
+      
     });
+    
   };
   
   return {
